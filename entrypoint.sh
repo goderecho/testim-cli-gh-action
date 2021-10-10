9d54598544be2964ebcf86d0824498296c390d7f
Mark Davidoff <markddavidoff@users.noreply.github.com>
2021-10-10 09:22:16 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if [ -z "$6" ]; then
+  parallel=1
+else
+  parallel=$6
+fi
+
 if [ -z "$5" ]; then
-  testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --parallel 20 
+  testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --parallel $parallel 
 else 
-   testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5 --parallel 20 
+   testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5 --parallel $parallel
 fi
