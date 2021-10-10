afdafc4cd3c61993f29e39187bed1096ed68e768
Mark Davidoff <markddavidoff@users.noreply.github.com>
2021-10-09 23:06:45 -0700

    try to add parallel flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$5" ]; then
-  testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4
+  testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --parallel 20 
 else 
-   testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5
+   testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5 --parallel 20 
 fi
