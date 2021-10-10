c0e4a7db6ac2e3a60def362442ebbc5d080546c8
Mark Davidoff <markddavidoff@users.noreply.github.com>
2021-10-09 22:56:53 -0700

    make testid optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5
+if [ -z "$5" ]; then
+  testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4
+else 
+   testim --token $1 --project $2 --grid $3 -r testim-report.xml --suite $4 --testId $5
+fi
